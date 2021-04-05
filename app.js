7fbc21d254492cd5161eea16339f945bfacc6d3a
Gantumur <gantumur0307@gmail.com>
2021-04-06 04:25:01 +0800

    event listeneruudiig uusgej ugluu

--- app.js
+++ app.js
@@ -11,18 +11,18 @@ var financeController = (function(){
 //programiin holbogch controller
 var appController = (function(uiController, financeController){
     var ctrlAddItem=function() {
-        //1. oruulah ugugdliig delgetsnees olj awna.
-    console.log("delgetsnees ugugdluu awah heseg");
-        //2. olj awsan ugugluudee sanhuugiin controllert damjuulj tend hadgalna.
-    
-        //3. olj awsan ugugdluugiig web deeree tohiroh hesegt gargana
-    
-        //4. tuswiig tootsoolno
-    
-        ////etsiin uldegdel, tootsoog delgetsend gargana
+    //1. oruulah ugugdliig delgetsnees olj awna.
+        console.log("delgetsnees ugugdluu awah heseg");
+    //2. olj awsan ugugluudee sanhuugiin controllert damjuulj tend hadgalna.
+
+    //3. olj awsan ugugdluugiig web deeree tohiroh hesegt gargana
+
+    //4. tuswiig tootsoolno
+
+    ////etsiin uldegdel, tootsoog delgetsend gargana
     }
     document.querySelector(".add__btn").addEventListener("click", function() {
-    ctrlAddItem();
+        ctrlAddItem();
 });
 document.addEventListener("keypress", function(event){
     if(event.keyCode === 13 || event.which===13) {
