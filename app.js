209c46e17cea02054d697af3bcb022fe0c72aa7e
Gantumur <gantumur0307@gmail.com>
2021-04-07 17:56:56 +0800

    finaciig hiij duuslaa

--- app.js
+++ app.js
@@ -1,18 +1,80 @@
 //delgesttei ajillah controller
 var uiController = (function(){
+var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn"
+};
+return {
+    getInput: function(){
+        return{
+            type: document.querySelector(DOMstrings.inputType).value, //inc, exp
+            description: document.querySelector(DOMstrings.inputDescription).value, //tailbar
+            value: document.querySelector(DOMstrings.inputValue).value //dun
+        };
+    },
+    getDOMstrings: function(){
+        return DOMstrings;
+    }
+}
 
 })();
 
 //sanhuutei ajillah controller
 var financeController = (function(){
-
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+      }
+      
+      var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+      }
+      
+      var i1 = new Income(1, 'Цалин', 2500000);
+      var i2 = new Income(1, 'Сугалаа хожсон', 25000000);
+      
+      // console.log(i1);
+      // console.log(i2);
+      
+      // var incomes = [];
+      // var expenses = [];
+      
+      // incomes.push(i1);
+      // incomes.push(i2);
+      
+      // console.log(incomes);
+      
+      // console.log(incomes[1].value)
+      
+      var data = {
+        allItems: {
+          inc: [],
+          exp: []
+        },
+      
+        totals: {
+          inc: 1000,
+          exp: 500
+        }
+      }
+      
+      data.allItems.inc.push(i1);
+      
+      console.log(data.allItems.inc[0])
 })();
 
 //programiin holbogch controller
 var appController = (function(uiController, financeController){
+
+
     var ctrlAddItem=function() {
     //1. oruulah ugugdliig delgetsnees olj awna.
-        console.log("delgetsnees ugugdluu awah heseg");
+    console.log(uiController.getInput()) ;
     //2. olj awsan ugugluudee sanhuugiin controllert damjuulj tend hadgalna.
 
     //3. olj awsan ugugdluugiig web deeree tohiroh hesegt gargana
@@ -21,7 +83,9 @@ var appController = (function(uiController, financeController){
 
     ////etsiin uldegdel, tootsoog delgetsend gargana
     }
-    document.querySelector(".add__btn").addEventListener("click", function() {
+var setupEventListener = function(){
+    var DOM = uiController.getDOMstrings();
+    document.querySelector(DOM.addBtn).addEventListener("click", function() {
         ctrlAddItem();
 });
 document.addEventListener("keypress", function(event){
@@ -29,5 +93,14 @@ document.addEventListener("keypress", function(event){
         ctrlAddItem();
     }
 });
+};
+
+return {
+    init: function(){
+        console.log("Application started...");
+        setupEventListener();
+    }
+}
 
 })(uiController, financeController);
+appController.init();
